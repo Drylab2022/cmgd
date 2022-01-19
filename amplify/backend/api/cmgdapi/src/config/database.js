786a1ea7920520d9996c12583514b8798904109e
QuanWa89 <qw.drylab@gmail.com>
2022-01-18 21:31:09 -0500

    deploy to aws

--- amplify/backend/api/cmgdapi/src/config/database.js
+++ amplify/backend/api/cmgdapi/src/config/database.js
@@ -1,7 +1,15 @@
-const user = process.env.DB_USER || "postgres";
-const host = process.env.DB_HOST || "localhost";
+// const user = process.env.DB_USER || "postgres";
+// const host = process.env.DB_HOST || "localhost";
+// const database = process.env.DB_NAME || "cmgd";
+// const password = process.env.DB_PWD || "docker";
+// const port = process.env.DB_PORT || 5432;
+
+const user = process.env.DB_USER || "drylab";
+const host =
+  process.env.DB_HOST ||
+  "cmgd-dev-cluster.cluster-cjxzpipgg2mq.us-east-1.rds.amazonaws.com";
 const database = process.env.DB_NAME || "cmgd";
-const password = process.env.DB_PWD || "docker";
+const password = process.env.DB_PWD || "wq890820";
 const port = process.env.DB_PORT || 5432;
 
 const { Sequelize } = require("sequelize");
