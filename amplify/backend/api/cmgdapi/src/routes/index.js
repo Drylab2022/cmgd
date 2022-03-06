5ac5a4fe6aafa883dcd3c294d464da4b4b1a4389
Tian <dingtian2019@163.com>
2022-03-06 16:17:59 -0500

    demo march 6

--- amplify/backend/api/cmgdapi/src/routes/index.js
+++ amplify/backend/api/cmgdapi/src/routes/index.js
@@ -3,15 +3,8 @@ const router = express.Router();
 
 const projectController = require("../controllers/").project;
 const sampleController = require("../controllers/").sample;
+const checkController = require("../controllers").check;
 
-/* GET home page. */
-// router.get("/", function (req, res, next) {
-//   res.render("index");
-// });
-
-router.get("/", (req, res) => {
-  res.send("hello");
-});
 /* Project Router */
 router.get("/api/project/:projectId/samples", projectController.listSamples);
 router.get("/api/project/:assignee", projectController.list);
@@ -22,5 +15,8 @@ router.post("/api/project", projectController.addWithSamples);
 /* Sample Router */
 router.get("/api/sample/:id", sampleController.getByPk);
 router.post("/api/samples", sampleController.upSert);
+
+//Check Router
+router.post("/api/check", checkController.check);
 
 module.exports = router;
