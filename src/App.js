1e7b4d63ebe828a7728bb6577549cf7719fb9bf7
Shilian Hou <hou.shi@northeastern.edu>
2022-04-21 15:10:43 -0400

    add forgetpassword and changeemail && Authenticated route

--- src/App.js
+++ src/App.js
@@ -47,7 +47,12 @@ function App() {
         <Navbar.Collapse className="justify-content-end">
           <Nav activeKey={window.location.pathname}>
             {isAuthenticated ? (
+              <>
+              <LinkContainer to="/settings">
+                <Nav.Link>Settings</Nav.Link>
+              </LinkContainer>
               <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              </>
             ) : (
               <>
                 <LinkContainer to="/signup">
