1317e1c47d88ba9f055981277e18d028a249e515
cuihongjian <cui.ho@northeastern.edu>
2022-05-20 14:13:24 -0700

    modify draftId type to Integer in ProjectProd table.

--- amplify/backend/api/cmgdapi/src/models/ProjectProd.js
+++ amplify/backend/api/cmgdapi/src/models/ProjectProd.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
       },
       draftId: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER,
         allowNull: false,
       },
     },
