cabce5e4a5f3b16890fbbe9c06be47d6446f0921
cuihongjian <cui.ho@northeastern.edu>
2022-06-16 09:59:10 -0700

    modify swagger docs

--- amplify/backend/api/cmgdapi/src/routes/production.js
+++ amplify/backend/api/cmgdapi/src/routes/production.js
@@ -47,11 +47,11 @@ router.post("/add", productionController.addProduction);
  *               filter:
  *                 type: string
  *                 description: search conditions, optional
- *                 example: {"and": [{"age": {"gt": 0}},{"age": {"lt": 100}}]}
+ *                 example: {"and": [{"age": {"gt": "0"}},{"age": {"lt": "100"}}]}
  *               timestamp:
  *                 type: string
  *                 description: search samples before this timestamp, optional, return latest samples in default without this parameter
- *                 example: {"lt": "2022-05-23"}
+ *                 example: {"lt": "2022-09-23"}
  *               page:
  *                 type: int
  *                 description: page number, index from 0
@@ -60,6 +60,10 @@ router.post("/add", productionController.addProduction);
  *                 type: int
  *                 description: total data number of current parameters
  *                 example: 10
+ *               cascading_filter:
+ *                 type: string
+ *                 description: cascading conditions, optional
+ *                 example: ["gender", "disease"]
  *             required:
  *               - page, count
  *     responses:
@@ -71,8 +75,41 @@ router.post("/add", productionController.addProduction);
 
 router.post("/search", productionController.search);
 
+/**
+ * @openapi
+ * /api/production/fields:
+ *   get:
+ *     tags:
+ *       - Production
+ *     summary: Return an array of properties from curation json
+ *     responses:
+ *       200:
+ *         description: Returns all properties
+ *       400:
+ *         description: Error happened in backend
+ */
 router.get("/fields", productionController.getAllFields);
 
+/**
+ * @openapi
+ * /api/production/fields/{field}:
+ *   get:
+ *     tags:
+ *       - Production
+ *     summary: Return an array of json objects
+ *     parameters:
+ *       - name: field
+ *         in: path
+ *         required: true
+ *         description: The field name to count and retrieve
+ *         type: string
+ *         example: gender
+ *     responses:
+ *       200:
+ *         description: Returns field related values with their count in an array
+ *       400:
+ *         description: Error happened in backend
+ */
 router.get("/fields/:field", productionController.countValue);
 
 module.exports = router;
